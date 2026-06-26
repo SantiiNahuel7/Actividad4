c34cd0a377dba63424955e5f1cfb681fb8326ca2
Santiago Orellana <santiagoorellana1905@gmail.com>
2026-06-26 18:59:20 -0300

    Correccion

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const form = document.getElementById('miFormulario');
 const inputNombre = document.getElementById('nombre');
 const inputEmail = document.getElementById('email');
 const inputEdad = document.getElementById('edad');
+const inputPais = document.getElementById('pais');
+const inputTerminos = document.getElementById('terminos');
+
 const errorNombre = document.getElementById('error-nombre');
 const errorEmail = document.getElementById('error-email');
 const errorEdad = document.getElementById('error-edad');
+const errorPais = document.getElementById('error-pais');
+const errorTerminos = document.getElementById('error-terminos');
 
 form.addEventListener('submit', function(e) {
-    
     let hayErrores = false;
 
-   
     if (inputNombre.value.trim().length < 5) {
         errorNombre.style.display = 'block';
         hayErrores = true;
@@ -34,8 +37,22 @@ form.addEventListener('submit', function(e) {
         errorEdad.style.display = 'none';
     }
 
+    if (inputPais.value === "") {
+        errorPais.style.display = 'block';
+        hayErrores = true;
+    } else {
+        errorPais.style.display = 'none';
+    }
+
+    if (!inputTerminos.checked) {
+        errorTerminos.style.display = 'block';
+        hayErrores = true;
+    } else {
+        errorTerminos.style.display = 'none';
+    }
+
+
     if (hayErrores) {
         e.preventDefault(); 
     }
-    
 });
